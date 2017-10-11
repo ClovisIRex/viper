4194867568c62a4334326c231ac97cf2ebf9b730
Vivek Joshy <daegontaven@gmail.com>
2017-10-11 12:18:55 -0400

    Multiple version support
    Add support for PyPy and other versions of CPython.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,13 @@
 import subprocess
 
+import sys
+
 
 def test_viper_command():
     """
     Ensure viper command exists.
     """
-    subprocess.run("viper", check=True)
+    if sys.version_info < (3, 5):
+        subprocess.call('viper')
+    else:
+        subprocess.run("viper")
